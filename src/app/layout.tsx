1ebffb94e53fae7f89c7130f3fde73e3b61fa1b9
Ajay Misra <ajaymisra1@outlook.com>
2025-06-23 12:23:22 -0500

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 // app/layout.tsx
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <body className={`${inter.className} min-h-screen bg-gray-950`}>
         {children}
+        <Analytics />
       </body>
     </html>
   )
